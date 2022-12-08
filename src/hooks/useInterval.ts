0cf0e4e77c4b9f107fd5702965facecd0a43a425
Caio Mori <caiomori22@gmail.com>
2022-12-08 09:56:04 -0300

    feat(useInterval): created hook

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,23 @@
+import {useEffect, useRef} from 'react'
+
+const useInterval = <C extends CallableFunction>(callback: C, delay: number | null) => {
+  const savedCallback = useRef<C>()
+
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    const tick = () => {
+      if (savedCallback.current) {
+        savedCallback.current()
+      }
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
+}
+
+export default useInterval
